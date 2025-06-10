ccd3b564718654e6d29931befd95002761f6ff24
Rahul Arulkumaran <rahuldravid313@gmail.com>
2025-06-10 17:26:27 -0400

    add additional refactor files

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+  ],
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    
+    // General rules
+    'no-console': 'off', // Allow console.log for debugging
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+    
+    // Code style
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': 'error',
+    'no-trailing-spaces': 'error',
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'agents/',
+    '*.js',
+    'webpack.*.js'
+  ],
+}; 
